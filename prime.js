aa6e247591481790bf6d68daabd8ed898318efd6
deepakdravid555-hub <deepakdravid555@gmail.com>
2026-07-25 11:25:02 +0530

    Update prime.js
    Implemented prime number checking logic and added console output.

--- prime.js
+++ prime.js
@@ -4,15 +4,20 @@
  * @returns {boolean} True if prime, false otherwise.
  */
 const isPrime = (num) => {
-    // TODO: 1. Handle edge cases (numbers <= 1) using const rules where applicable
-    
-    // TODO: 2. Handle the number 2 and even numbers
-    
-    // TODO: 3. Use a loop to check for factors up to the square root of num
-    // Hint: Use 'const' for values that stay the same and 'let' for values that change
-    
-    return false; // Change this return statement
-};
+    if(num <= 1){
+        return false;
+    }
+    for(let i = 2; i < num; i++){
+        if(num % i === 0){
+            return false
+        }
+    }
+    return true;
+}
 
-// Export the function for the test cases
-module.exports = isPrime;
+let number = 15;
+if(isPrime(number)){
+    console.log(number + "is a prime number");
+}else{
+    console.log(number + "is not a prime number");
+}
